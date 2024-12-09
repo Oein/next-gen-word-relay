ff3760b37f78c8c12d42bad8955fe397d5e580f5
Oein <oein0219@gmail.com>
2024-12-10 00:20:56 +0900

    feat(database): :card_file_box: add words collection

--- packages/common-backend/src/db/index.ts
+++ packages/common-backend/src/db/index.ts
@@ -1,13 +1,27 @@
-import { MongoClient } from "mongodb";
+import { Db, MongoClient } from "mongodb";
 import logger from "../logger";
 
 const url = process.env.MONGO_URI || "mongodb://localhost:27017";
 const client = new MongoClient(url);
 
+let db: Db;
+
+export async function ensureIndexes() {
+  await getWordsCollection().createIndex(
+    { word: 1 },
+    {
+      unique: true,
+    }
+  );
+}
+
 export async function connect() {
   logger.loading("Connecting to MongoDB");
   try {
     await client.connect();
+    db = client.db("game");
+
+    await ensureIndexes();
   } catch (e) {
     logger.error("Failed to connect to MongoDB");
     logger.error(e);
@@ -16,6 +30,25 @@ export async function connect() {
     return;
   }
   logger.success("Connected to MongoDB");
+}
+
+export async function disconnect() {
+  logger.loading("Disconnecting from MongoDB");
+  await client.close();
+  logger.success("Disconnected from MongoDB");
+}
+
+// Handle before closing
+process.on("beforeExit", async () => {
+  await disconnect();
+});
+
+export function getWordsCollection() {
+  return db.collection<{
+    word: string;
+    lang: number;
+    tags: string[];
+  }>("words", {});
 }
 
 export default client;
